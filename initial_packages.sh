588e67c2869ab61228a0af24002235976b217a54
Jimmy <jimmy@pop-os.localdomain>
2020-06-01 12:08:04 -0400

    Added nvim setup and nodejs to included software for coc.vim

--- initial_packages.sh
+++ initial_packages.sh
@@ -1,10 +1,11 @@
 pkgMan="apt" #TODO: Make this argument so it can be chosen from setup.sh
 
 sudo $pkgMan install \
-	neovim \
-	fish \
-    curl \
-    cowsay \
-    fortune \
-    fonts-powerline \
+    neovim \
+    fish \
+    curl \ # for fish setup
+    cowsay \ # for fun
+    fortune \ # for fun
+    fonts-powerline \ # for fish themes
+    nodejs \ # for coc.vim
     -y
--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 source initial_updates.sh
 source initial_packages.sh
 source gogh_shell_colors.sh
-# TODO: nvim
-#source vim/setup_vim.sh
+source nvim/setup_nvim.sh
 source fish/setup_fish.sh # Must be last step bc we enter fish shell
 
