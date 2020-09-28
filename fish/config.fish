34d20412df862cae1d01732e9f1e9c730c64d3af
Jimmy <silvaj1503@gmail.com>
2020-09-27 20:32:23 -0400

    Moved fish stuff to seperate repo

--- fish/config.fish
+++
@@ -1,1 +0,0 @@
-alias update='sudo apt update ; sudo apt -y upgrade ; sudo apt -y dist-upgrade; sudo apt autoremove -y'
--- fish/setup_fish.sh
+++
@@ -1,9 +0,0 @@
-# Add the fish shell /usr/local/bin/fish to /etc/shells with if it's not there:
-# echo /usr/bin/fish | sudo tee -a /etc/shells
-
-# change your default shell to fish with:
-chsh -s /usr/bin/fish
-
-# Install oh-my-fish: https://github.com/oh-my-fish/oh-my-fish 
-# also creates ~/.config/fish
-curl -L https://get.oh-my.fish | fish
