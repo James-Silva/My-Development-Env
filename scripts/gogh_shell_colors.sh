b7ee2b66b82ec84ffe9803c39890b29e18c4211d
Jimmy <jsilva@veson.com>
2024-08-09 15:30:25 -0400

    Removed unused file

--- scripts/gogh_shell_colors.sh
+++
@@ -1,6 +0,0 @@
-### Find Console Colors ###
-# https://mayccoll.github.io/Gogh/
-# RaspberryPi Bug Workaround: https://github.com/Mayccoll/Gogh/issues/63
-
-echo ALL | bash -c  "$(wget -qO- https://git.io/vQgMr)" 
-
